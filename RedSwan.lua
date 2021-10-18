d3550cde7caff3ecfe005f1136dbb22a90a4dbb9
ItsODRJR <69770237+ItsODRJR@users.noreply.github.com>
2021-10-18 15:40:47 -0500

    Delete RedSwan.lua

--- RedSwan.lua
+++
@@ -1,9 +0,0 @@
-wait(0.1)
-print("hey")
-workspace.CurrentCamera.CameraType = Enum.CameraType.Scriptable
-workspace.CurrentCamera.CFrame = workspace.CameraA.CFrame
-game.StarterGui.Sound:Play()
-game:GetService("RunService").RenderStepped:Connect(function()
-local boom = game.StarterGui.Sound.PlaybackLoudness / 175
-workspace.Part.PointLight.Brightness = boom * 1.25
-end)
